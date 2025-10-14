33ea0b99a1aec81794a536f567ee14cc640e3439
andre-saccoo <andre.sacco@icloud.com>
2025-10-14 14:00:21 +0200

    salvataggio

---
+++ cabina.py
@@ -0,0 +1,28 @@
+class Cabina:
+    def __init__(self,codiceCabina, numeriPosti, ponteNave, prezzo):
+        self._codiceCabina=codiceCabina
+        self._numeroPosti=numeriPosti
+        self._ponteNave=ponteNave
+        self._prezzo=prezzo
+        self._disponibile=True
+
+    @property
+    def disponibile(self):
+        return self._disponibile
+    @disponibile.setter
+    def disponibile(self,disponibile):
+        self._disponibile=disponibile
+
+    def __str__(self):
+        return f"Codice cabina: {self._codiceCabina}\n" \
+               f"numero posti: {self._numeroPosti}\n"\ 
+               f"Ponte nave: {self._ponteNave}"\
+               f"prezzo: {self._prezzo}"
+
+    class Cabina_deluxe(Cabina):
+        def __init__(self, codiceCabina, numeriPosti, ponteNave, prezzo, tipologia):
+            super.__init__(codiceCabina, numeriPosti, ponteNave, prezzo, disponibile)
+            self._tipologia=tipologia
+
+            def sovrapprezzo (self, prezzo):
+                return self.prezzo * 1.2
