a430c6cae17ef8e72dad624f0a0c86f0cb08a4ca
andre-saccoo <andre.sacco@icloud.com>
2025-10-20 19:17:28 +0200

    salvataggio

--- crociera.py
+++ crociera.py
@@ -2,50 +2,48 @@ import  csv
 from passeggero import Passeggero
 from cabina import Cabina, Cabina_deluxe, Cabina_animale
 
+#definisco la classe cabina che gestisce anche le classi cabina (di vario tipo) e la classe passeggero
 class Crociera:
-    def __init__(self, nome):
+    def __init__ (self, nome):
         self._nome=nome
-# per i passeggeri e per le cabine creo sia una lista che un dizionario,
-#le liste sono più veloci nell'ordinamento, i dizionari nella ricerca
+
+        # per i passeggeri e per le cabine creo sia una lista che un dizionario,
+        #le liste sono più veloci nell'ordinamento, i dizionari nella ricerca
         self.listaPasseggeri=[]
         self.dizionarioPasseggeri=dict()
         self.listaCabine=[]
         self.dizionarioCabina=dict()
 
-#definisco il metodo setter e getter per poter leggere i dati della nave
+    #definisco il metodo setter e getter per poter leggere i dati della nave
     @property
     def nome (self):
         return self._nome
     @nome.setter
-    def nome(self, nome):
+    def nome (self, nome):
         self._nome = nome
 
-# funzione che prova ad aprire il file, per ogni riga controlla la lunghezza
-# se la lunghezza della riga è 3 sto leggendo un passeggero, controllo non sia già presente nel dizionario dedicato
-# se non è presente lo aggiungo sia alla lista che al dizionario dei passeggeri
-# se la lunghezza della riga è 4 sto leggendo una cabina standard, controllo non sia già presente nel dizionario dedicato
-# se non è presente la aggiungo sia alla lista che al dizionario delle cabine
-# se la lunghezza della riga è 5 provo a convertire il quinto elemento in intero, se riesco è una cabina per animali e la
-# aggiungo alla lista e al dizionario delle cabine se non è già presente, se non riesco a convertire attraverso le
-# eccezioni capisco il tipo e aggiungo la cabina di tipo deluxe alla lista e al dizionario delle cabine
-
-    def carica_file_dati(self, file_path):
+    #DUCK TYPING
+    # funzione che prova ad aprire il file, per ogni riga controlla la lunghezza
+    def carica_file_dati (self, file_path):
         try:
-            infile=open(file_path)
-            reader = csv.reader(infile)
+            infile=open (file_path)
+            reader = csv.reader (infile)
             for row in reader:
-                print(row)
+                print (row)
                 id= row[0].upper()
+
+                # se la lunghezza della riga è 3 sto leggendo un passeggero, controllo anche chi il codice passeggero inizi con P, controllo non sia
+                #già presente nel dizionario dei passeggeri, se non è presente lo aggiungo sia alla lista che al dizionario dei passeggeri
                 if len(row)==3 and id.startswith("P"):
                     id, nome, cognome= row[0], row[1], row[2]
                     if id in self.dizionarioPasseggeri:
                         raise ValueError(f" passeggero {id} già presente ")
-                    passeggero = Passeggero(id, nome, cognome)
+                    passeggero = Passeggero (id, nome, cognome)
                     self.listaPasseggeri.append(passeggero)
                     self.dizionarioPasseggeri[id] = passeggero
 
-
-
+                # se la lunghezza della riga è 4 sto leggendo una cabina standard, controllo che il codice cominci con CAB, controllo non sia già presente
+                # nel dizionario dedicato, se non è presente la aggiungo sia alla lista che al dizionario delle cabine
                 if len(row)==4 and id.startswith("CAB"):
                     id, numLetti, numPonte, PrezzoBase= row[0], row[1], row[2], row[3]
                     if id in self.dizionarioCabina:
@@ -54,6 +52,9 @@ class Crociera:
                     self.listaCabine.append(cabina)
                     self.dizionarioCabina[id] = cabina
 
+                # se la lunghezza della riga è 5 e il codice cabina comincia con CAB, provo a convertire il quinto elemento in intero,
+                # se riesco è una cabina per animali e la aggiungo alla lista e al dizionario delle cabine se non è già presente, se non riesco a
+                # convertire attraverso le eccezioni capisco il tipo e aggiungo la cabina di tipo deluxe alla lista e al dizionario delle cabine
 
                 if len(row)==5 and id.startswith("CAB"):
                     try:
@@ -72,28 +73,36 @@ class Crociera:
                         self.listaCabine.append(cabinaDeluxe)
                         self.dizionarioCabina[id] = cabinaDeluxe
             infile.close()
+        # gestisco l'eccezione nel caso il file non venga trovato
         except FileNotFoundError:
-            print(" File not found ")
+            print (" File not found ")
+
+        #stampo i dizionari contenti i dati di cabine e passeggeri come verifica
+        for id, passeggero in self.dizionarioPasseggeri.items():
+            print ( passeggero )
+
         for id, cabina in self.dizionarioCabina.items():
-            print(cabina)
+            print ( cabina )
 
-    def assegna_passeggero_a_cabina(self, codice_cabina, codice_passeggero):
+    # al metodo vengono passati il codice cabina e il codice passeggero, controllo che esistano nel sistema, se la cabina è disponibile
+    # se supera i controlli rendo la cabina non disponibile, salvo i dati di passeggero e cabina nel sistema
+    def assegna_passeggero_a_cabina (self, codice_cabina, codice_passeggero):
         if codice_cabina not in self.dizionarioCabina:
-            raise ValueError (" la cabina richiesta non esiste nel sistema")
+            raise ValueError ( " la cabina richiesta non esiste nel sistema" )
         if codice_passeggero not in self.dizionarioPasseggeri:
-            raise ValueError (" il passeggero non esiste nel sistema")
+            raise ValueError ( " il passeggero non esiste nel sistema" )
 
-        cabina=self.dizionarioCabina[codice_cabina]
-        passeggero=self.dizionarioPasseggeri[codice_passeggero]
+        cabina=self.dizionarioCabina [codice_cabina]
+        passeggero=self.dizionarioPasseggeri [codice_passeggero]
 
         if cabina.disponibile == False:
-            print(" la cabina richiesta non è disponibile ")
+            print ( " la cabina richiesta non è disponibile ")
         else:
             cabina.passeggero=passeggero
             passeggero.cabina=cabina
             cabina.disponibile=False
 
-
+    #controllo che i dati siano già stati caricati nel sistema, se la lista è piena la ordino in base al prezzo
     def cabine_ordinate_per_prezzo(self):
             if len(self.listaCabine)>0:
                 cabineOrdinate = sorted(self.listaCabine, key=lambda c: c.prezzoFinale())
--- main.py
+++ main.py
@@ -1,61 +1,68 @@
+#importo la classe crociere che gestisce tutto il sistema
 from crociera import Crociera
 
-def menu():
-    print(f'\n--- MENU CROCIERA ---')
-    print("1. Modifica nome della crociera")
-    print("2. Carica dati da file")
-    print("3. Assegna cabina a passeggero")
-    print("4. Visualizza cabine ordinate per prezzo")
-    print("5. Visualizza elenco passeggeri")
-    print("6. Esci")
-
-    return input("Scegli un'opzione >> ")
+#stampo il menù con le opzioni che l'utente può scegliere
+def menu ():
+    print( f'\n--- MENU CROCIERA ---' )
+    print( "1. Modifica nome della crociera" )
+    print( "2. Carica dati da file" )
+    print( "3. Assegna cabina a passeggero" )
+    print( "4. Visualizza cabine ordinate per prezzo" )
+    print( "5. Visualizza elenco passeggeri" )
+    print( "6. Esci" )
+    return input( "Scegli un'opzione: " )
 
 def main():
-    crociera = Crociera("MSC Futura")
+    #definisco il nome della crociera da codice che l'utente può poi modificare scegliendo l'opzione 1
+    crociera = Crociera( "MSC Futura" )
 
+    #fino a che l'utente non sceglie di uscire con il comando 6 il programma non si ferma
     while True:
-        scelta = menu()
-        # controllare
-        if scelta == "1":
-            nuovo_nome = input("Inserisci il nuovo nome della crociera: ")
-            crociera.nome= nuovo_nome
-            print(f"il nuovo nome della crociera è: {crociera.nome}")
+        scelta = menu ()
 
+        #cambio nome alla nave
+        if scelta == "1":
+            nuovo_nome = input ( "Inserisci il nuovo nome della crociera: " )
+            crociera.nome = nuovo_nome
+            print ( f"il nuovo nome della crociera è: {crociera.nome}" )
+
+        # carico i dati dal file path dando conferma all'utente o segnalando errore, la funzione carica_file_dati è definita in crociera.py
         elif scelta == "2":
             file_path = "dati_crociera.csv"
             try:
-                crociera.carica_file_dati(file_path)
-                print("Dati caricati correttamente.")
+                crociera.carica_file_dati (file_path)
+                print ( "Dati caricati correttamente." )
             except FileNotFoundError:
-                print("File non trovato.")
+                print ( "File non trovato." )
 
+        # chiedo all'utente di inserire un codice cabina e un codice passeggero, li rendo maiuscoli con il metodo .upper() e li passo alla funzione
         elif scelta == "3":
-            codice_cabina = input("Codice cabina: ")
-            codiceCabina=codice_cabina.upper()
-            codice_passeggero = input("Codice passeggero: ")
+            codice_cabina = input ( "Codice cabina: ")
+            codiceCabina = codice_cabina.upper()
+            codice_passeggero = input ( "Codice passeggero: ")
             codicePasseggero = codice_passeggero.upper()
             try:
                 crociera.assegna_passeggero_a_cabina(codiceCabina, codicePasseggero)
-                print("Cabina assegnata con successo.")
+                print ( "Cabina assegnata con successo." )
             except Exception as e:
-                print(f"Errore: {e}")
+                print ( f"Errore: {e}" )
 
+        # stampo la lista delle cabine ordinate per prezzo finale
         elif scelta == "4":
             crociera.cabine_ordinate_per_prezzo()
-            print("\n--- Cabine ordinate per prezzo ---")
+            print ( "\n--- Cabine ordinate per prezzo ---" )
 
 
         elif scelta == "5":
-            print("\n--- Elenco passeggeri ---")
+            print ( "\n--- Elenco passeggeri ---" )
             crociera.elenca_passeggeri()
 
         elif scelta == "6":
-            print("Uscita dal programma.")
+            print ( "Uscita dal programma." )
             break
 
         else:
-            print("Scelta non valida.")
+            print ( "Scelta non valida." )
 
 if __name__ == "__main__":
     main()
